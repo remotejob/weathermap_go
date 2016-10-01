3b0aaabf1b5a89cd0f19e8f10ca65689edf8aff2
juno <juno@krasava.fi>
2016-10-01 05:15:59 +0300

    dev

---
+++ server.go
@@ -0,0 +1,26 @@
+//go:generate  /home/juno/neonworkspace/gowork/bin/statik -src=/home/juno/git/github.com/remotejob/weathermap_ang2_webpack/dist
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/rakyll/statik/fs"
+	_ "github.com/remotejob/weathermap_go/statik"
+)
+
+func main() {
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	// // fs := http.FileServer(http.Dir("/home/juno/neonworkspace/gowork/src/github.com/remotejob/godocker/assets"))
+	// fs := http.FileServer(http.Dir("assets"))
+
+	// http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	// http.Handle("/assets", http.FileServer(http.Dir("/home/juno/neonworkspace/gowork/src/github.com/remotejob/godocker/assets")))
+	http.Handle("/", http.FileServer(statikFS))
+	http.ListenAndServe(":8080", nil)
+}
