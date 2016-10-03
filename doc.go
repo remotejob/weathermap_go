46321e67a7676f998f1f27a82e7ed08b684d396c
juno <juno@krasava.fi>
2016-10-03 06:19:39 +0300

    dev

--- doc.go
+++ doc.go
@@ -13,9 +13,9 @@
 	The verbs:
 
 	General:
-		%v	the value in a default format
-			when printing structs, the plus flag (%+v) adds field names
-		%#v	a Go-syntax representation of the value
+		%vthe value in a default format
+		    when printing structs, the plus flag (%+v) adds field names
+		%#v a Go-syntax representation of the value
 		%T	a Go-syntax representation of the type of the value
 		%%	a literal percent sign; consumes no value
 */
